7b9fc4ffcd0e711c0ff122a9712c19489a109e05
Mauro Brandão <mrbrjr@gmail.com>
2025-06-29 09:02:49 -0300

    Refactor Timer1 ISR
    Refactor Timer1 ISR to use modular pulse control functions. The ISR
    now delegates logic to dedicated functions like `begin_pulse()`,
    `end_pulse()`, `begin_offtime()` and `end_offtime()`, which is
    cleaner.  Improved variable naming and moved relevant globals
    into the cfg struct for better encapsulation.

--- main.c
+++ main.c
@@ -8,27 +8,29 @@
 #define PRESCALER_VALUE 1024
 #define MAX_16BIT   65535
 
-
 //volatile uint8_t pulse_requested = 0;
 typedef struct{
-    uint8_t mode; 
-    uint16_t duration;
-    uint16_t period;
-    uint8_t count;
-    uint8_t pending;
-    uint8_t running;
+    volatile uint8_t mode; 
+    volatile uint32_t duration;
+    volatile uint32_t period;
+    volatile uint16_t pulse_counter;
+    volatile uint8_t required_pulses;
+    volatile uint8_t pending;
+    volatile uint8_t running;
+    volatile uint8_t in_pulse;
 } pulse_config;
 
 volatile pulse_config cfg = {
     .mode = 0,
     .duration = 100,
     .period = 1000,
-    .count = 1,
+    .pulse_counter = 0,
+    .required_pulses = 3,
     .pending = 0,
     .running = 0,
+    .in_pulse = 0,
 };
 
-volatile uint8_t pulse_counter = 0;
 volatile char serial_buffer[15];
 volatile uint8_t serial_index = 0;
 
@@ -39,8 +41,8 @@ void setup_timer1(void) {
     // CTC mode (Resets on OCR1A match)
     TCCR1B = (1 << WGM12); 
     
-    // Enables compare match interrupt
-    TIMSK1 = (1 << OCIE1A);
+    // Enables compare match and overflow interrupts
+    TIMSK1 = (1 << OCIE1A); // |  (1 << TOIE1);
 }
 
 void setup_serial(){
@@ -55,66 +57,84 @@ void start_pulse(){
     PORTD |= (1 << OUTPUT_PIN);
     PORTB |= (1 << LED_PIN);
     
-    cfg.running = 1;
-
     // Reset timer
     TCNT1 = 0;
     
+    // Compute the number of ticks needed 
     uint32_t ticks = (F_CPU/1000)*cfg.duration/PRESCALER_VALUE - 1;
+
+    // Set the Timer1 match value
     OCR1A = ticks > MAX_16BIT ? MAX_16BIT : ticks;
 
-    // Selects Timer1 prescaler 
-    TCCR1B |=  (1 << CS12) | (1 << CS10); 
+    // Restart Timer1 
+    TCCR1B |=  (1 << CS12) | (1 << CS10);
+    
+    // Until a new interrupt occurs, we are in a positive part of the pulse
+    cfg.in_pulse = 1;
 }
 
-ISR(TIMER1_COMPA_vect){
-    // This tracks if we are about to end a pulse or enter a new pulse
-    // in multi mode. If in_pulse == 1, then we are at the end of a pulse
-    static uint8_t in_pulse = 1;
+void end_pulse(void){
+    // End pulse
+    PORTD &= ~(1 << OUTPUT_PIN);
+    PORTB &= ~(1 << LED_PIN);
+
+    // Stop timer
+    TCCR1B &= ~((1 << CS12) | (1 << CS10));
     
-    if (in_pulse){
-        // End pulse
-        PORTD &= ~(1 << OUTPUT_PIN);
-        PORTB &= ~(1 << LED_PIN);
-        
-        // Single mode
-        if (cfg.mode == 0){
-            cfg.running = 0;
+    // Until a new interrupt occurs, we are outside the positive part of the pulse
+    cfg.in_pulse = 0;
+}
 
-            // Stop timer
-            TCCR1B &= ~((1 << CS12) | (1 << CS10));
-        
-        // Either Continuous or Multi mode
-        }else{
-            // Prepare the next pulse
-            if ((++pulse_counter < cfg.count && cfg.mode == 1) || (cfg.mode == 2)){
-                // Calculate OFF time
-                uint16_t off_time = cfg.period - cfg.duration;
-
-                // Schedule next pulse after period
-                uint32_t ticks = (F_CPU/1000)*off_time/PRESCALER_VALUE - 1;
-                OCR1A = ticks > MAX_16BIT ? MAX_16BIT : ticks;
-                TCNT1 = 0;
+void start_offtime(void){
+    // Reset Timer1
+    TCNT1 = 0;
             
-                // Restart timer
-                TCCR1B |= (1 << CS12) | (1 << CS10);
-                
-                // Set in_pulse to 0, so that the next ISR will start a new pulse
-                in_pulse = 0;
-            // End of last pulse in Multi mode
-            }else{
-                cfg.running = 0;
-                pulse_counter = 0;
+    // Compute the number of ticks needed
+    uint32_t off_time = cfg.period - cfg.duration;
+    uint32_t ticks = (F_CPU/1000)*off_time/PRESCALER_VALUE - 1;
 
-                // Stop timer
-                TCCR1B &= ~((1 << CS12) | (1 << CS10)); 
-            }
-        }
-    // Start a new pulse in Multi or Continuous modes
+    // Set Timer1
+    OCR1A = ticks > MAX_16BIT ? MAX_16BIT : ticks;
+
+    // Restart Timer1 
+    TCCR1B |=  (1 << CS12) | (1 << CS10);
+
+}
+
+void end_offtime(void){
+    // Stop timer
+    TCCR1B &= ~((1 << CS12) | (1 << CS10));
+    
+    // Increment pulse counter
+    cfg.pulse_counter++;
+}
+
+
+ISR(TIMER1_COMPA_vect){
+    if (cfg.in_pulse){
+        // Do stuff related to ending a pulse
+        end_pulse();
+
+        // Prepare next off time
+        start_offtime();
     }else{
-        start_pulse();
-        in_pulse = 1;
-    }      
+        // Do stuff related to ending an off time
+        end_offtime();
+
+        // Prepare next pulse
+        if ((cfg.pulse_counter < cfg.required_pulses && cfg.mode == 1) || (cfg.mode == 2)){
+            start_pulse();
+            cfg.running = 1;
+        
+        // It was the last off time. End it
+        }else{
+            // Stop timer
+            //TCCR1B &= ~((1 << CS12) | (1 << CS10));
+
+            cfg.running = 0;
+            cfg.pulse_counter = 0;
+        }
+    } 
 }
 
 ISR(USART_RX_vect){
@@ -130,6 +150,9 @@ ISR(USART_RX_vect){
     if (data == '\n'){
         process_command();
         serial_index = 0;
+        for (uint8_t i = 0; i<15; i++){
+            serial_buffer[i] = 0;
+        }
     // Fill the command buffer
     }else if(serial_index < 15){
         serial_buffer[serial_index++] = data;
@@ -161,9 +184,9 @@ void process_command(){
             //}
             break;
 
-        // count
+        // required_pulses
         case 'c':
-            cfg.count = atoi(&serial_buffer[1]);
+            cfg.required_pulses = atoi(&serial_buffer[1]);
             break;
 
         // trigger
@@ -200,13 +223,15 @@ int main() {
     DDRB |= (1 << LED_PIN);
     DDRD |= (1 << OUTPUT_PIN);
     
+    // Signal initialization  
+    PORTB |= (1 << LED_PIN);
+
     setup_timer1();
     setup_serial();
     sei();
     
     // Signal that initialization is done
-    PORTB |= (1 << LED_PIN);
-    _delay_ms(1000);
+    _delay_ms(500);
     PORTB &= ~(1 << LED_PIN);
 
     while (1){
